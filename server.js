22aadd14e12f280a03ffc2c93712dcf856e12168
Satish Prajapati <72158548+satishpwebdev@users.noreply.github.com>
2025-01-26 22:30:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
-const Routes = require('./routes/route')
+const {MagnetRouter, SeedrRouter} = require('./routes/route')
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/magnet', MagnetRouter);
+app.use('/seedr', SeedrRouter);
 
-app.use('/', Routes)
+// app.use('/', Routes)
 
 app.get('/',(req,res)=>{
+   console.log(Routes)
    res.send("Radhe Radhe !!")
 })
 
