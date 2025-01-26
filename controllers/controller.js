f39b217c9e4616f2725724724368d7d28dce95a0
Satish Prajapati <72158548+satishpwebdev@users.noreply.github.com>
2025-01-26 22:04:31 +0530

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,53 @@
 
 
- class Controller {
-   constructor(response) {
-      this.response = response;
-   }
+//  class Controller {
+//    constructor(response) {
+//       this.response = response;
+//    }
 
-   sendResponse(data) {
-      this.response.status(200).json(data);
-   }
+//    sendResponse(data) {
+//       this.response.status(200).json(data);
+//    }
 
-   sendError(data){
-      this.response.status(409).json(data);
+//    sendError(data){
+//       this.response.status(409).json(data);
 
-   }
+//    }
+// }
+
+
+class Controller {
+  constructor(response) {
+    this.response = response;
+  }
+
+  sendResponse(resOrData) {
+    if (
+      resOrData &&
+      resOrData.status &&
+      typeof resOrData.status === "function"
+    ) {
+      return resOrData.status(200).json(arguments[1]);
+    }
+    if (this.response) {
+      return this.response.status(200).json(resOrData);
+    }
+    throw new Error("No response object available");
+  }
+
+  sendError(resOrData) {
+    if (
+      resOrData &&
+      resOrData.status &&
+      typeof resOrData.status === "function"
+    ) {
+      return resOrData.status(409).json(arguments[1]);
+    }
+    if (this.response) {
+      return this.response.status(409).json(resOrData);
+    }
+    throw new Error("No response object available");
+  }
 }
 
 
